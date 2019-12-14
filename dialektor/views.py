04a636a66173c4d0b7cd8529e4eb7e8031a87a2f
yasharAhari <yashardmc@gmail.com>
2019-12-14 01:14:38 -0600

    added signup form function

--- dialektor/views.py
+++ dialektor/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index_home(request):
     # main entry page (aka. login page)
     return render(request, 'login.html')
+
+
+def signup(request):
+    # renders the signup form
+    return render(request, 'signup.html')
