bd5ff2c73f69d500e04aad54c27362e673c44bfc
yasharAhari <yashardmc@gmail.com>
2019-12-14 02:02:18 -0600

    added signup page function

--- dialektor/views.py
+++ dialektor/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index_home(request, second):
+def index_home(request, second=None):
     # main entry page (aka. login page)
+    # The "second" argument is not used here, however, it is needed for the group regex call on urls
     return render(request, 'login.html')
 
 
